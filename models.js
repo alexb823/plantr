3c400535c4ff15929d73f8ff0fed8465487e6bd1
Alex Bukhman <alexb823@gmail.com>
2019-02-15 22:19:12 -0500

    finished seeding

--- models.js
+++ models.js
@@ -1,37 +1,34 @@
 const Sequelize = require('sequelize');
 
-// const db = new Sequelize('postgres://localhost:5432/plantr', { logging: false });
+const db = new Sequelize('postgres://localhost:5432/plantr', { logging: false });
 
-// For cloud9 db
-const db = new Sequelize('ubuntu', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
-});
+// // For cloud9 db
+// const db = new Sequelize('ubuntu', 'postgres', 'password', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   logging: false,
+// });
 
 const Gardener = db.define('gardener', {
-    name: Sequelize.STRING,
-    age: Sequelize.INTEGER
-})
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER,
+});
 
 const Plot = db.define('plot', {
-    size: Sequelize.INTEGER,
-    shaded: Sequelize.BOOLEAN
-})
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN,
+});
 
 const Vegetable = db.define('vegetable', {
-    name: Sequelize.STRING,
-    color: Sequelize.STRING,
-    planted_on: Sequelize.DATE
-})
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE,
+});
 
 Plot.belongsTo(Gardener);
 Gardener.hasOne(Plot);
 Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
 Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
-Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
+Gardener.belongsTo(Vegetable, { as: 'favoriteVegetable' });
 
 module.exports = { db, Gardener, Plot, Vegetable };
-
-
-
--- seed.js
+++ seed.js
@@ -1,26 +1,67 @@
 const { db, Gardener, Plot, Vegetable } = require('./models.js');
 
-
 db.sync({ force: true })
   .then(() => console.log('Database synced!'))
   .then(() => {
-    return Vegetable.create({
-      name: 'tomato',
-      color: 'red',
-      planted_on: new Date('February 14, 2019')
-    })
+    return Promise.all([
+      Vegetable.create({
+        name: 'tomato',
+        color: 'red',
+        planted_on: new Date('April 3, 2019'),
+      }),
+      Vegetable.create({
+        name: 'carrot',
+        color: 'orange',
+        planted_on: new Date('April 4, 2019'),
+      }),
+      Vegetable.create({
+        name: 'potato',
+        color: 'yellow',
+        planted_on: new Date('March 29, 2019'),
+      }),
+    ]);
   })
-  .then((vegetable) => {
-    return Gardener.create({
-      name: 'Prof',
-      age: 54,
-      favoriteVegetableId: vegetable.id
-    })
+  .then(([tomato, carrot, potato]) => {
+    return Promise.all([
+      Gardener.create({
+        name: 'moe',
+        age: 121,
+        favoriteVegetableId: carrot.id,
+      }),
+      Gardener.create({
+        name: 'larry',
+        age: 116,
+        favoriteVegetableId: tomato.id,
+      }),
+      Gardener.create({
+        name: 'curly',
+        age: 115,
+        favoriteVegetableId: carrot.id,
+      }),
+      Gardener.create({
+        name: 'shemp',
+        age: 123,
+        favoriteVegetableId: potato.id,
+      }),
+    ]);
   })
-  .then((gardener) => {
-    return
+  .then(([moe, larry, curly, shemp]) => {
+    return Promise.all([
+      Plot.create({ size: 4, shaded: false, gardenerId: moe.id }),
+      Plot.create({ size: 3, shaded: true, gardenerId: larry.id }),
+      Plot.create({ size: 5, shaded: false, gardenerId: curly.id }),
+      Plot.create({ size: 2, shaded: true, gardenerId: shemp.id }),
+      Vegetable.findAll(),
+    ]);
+  })
+  .then(([moePlot, larryPlot, curlyPlot, shempPlot, vegetables]) => {
+    const [tomato, carrot, potato] = vegetables;
+    return Promise.all([
+      moePlot.addVegetables([tomato, potato]),
+      larryPlot.addVegetable(carrot),
+      curlyPlot.addVegetables([carrot, potato]),
+      shempPlot.addVegetable(tomato),
+    ]);
   })
   .catch(err => console.error(err))
-  .finally(() => {
-    db.close()
-  })
+  .finally(() => db.close());
